784f9be6cd8f11a8bd3e4b00baa8a8c66c87d27c
Florian Zouhar <florian.zouhar@igd.fraunhofer.de>
2019-07-04 17:22:59 +0200

    redux dev mode only in dev env

--- src/actions/store.js
+++ src/actions/store.js
@@ -1,5 +1,10 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers/root";
 
-export default createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+let store = createStore(rootReducer);
+
+if (process.env.NODE_ENV === 'development') {
+    store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+}
+export default store;
 
