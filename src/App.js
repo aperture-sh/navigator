12661ef8d9fa7116b90055c5769601680e0317b0
Florian Zouhar <florian.zouhar@igd.fraunhofer.de>
2019-08-08 09:19:42 +0200

    theming added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Map from "./components/Map";
 import Sidebar from "./components/Sidebar";
 import FileUpload from "./logic/FileUpload";
@@ -8,17 +8,6 @@ import {configChange} from "./actions/Actions";
 import Exhauster from "./components/Exhauster";
 import NavigatorDialog from "./components/NavigatorDialog";
 
-import '@material/react-button/dist/button.css'
-import '@material/react-list/dist/list.css';
-import "@material/react-dialog/dist/dialog.css";
-import '@material/react-card/dist/card.css';
-import '@material/react-text-field/dist/text-field.css';
-import '@material/react-top-app-bar/dist/top-app-bar.css';
-import '@material/react-material-icon/dist/material-icon.css';
-import '@material/react-layout-grid/dist/layout-grid.css';
-import "@material/react-switch/dist/switch.css";
-import '@material/react-typography/dist/typography.css';
-import "@material/react-drawer/dist/drawer.css";
 import AppBar from "./components/AppBar";
 import {TopAppBarFixedAdjust} from '@material/react-top-app-bar';
 import Drawer, {DrawerAppContent, DrawerContent} from "@material/react-drawer";
