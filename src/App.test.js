94ae8a07217081ae766b1dac4b3f61ef351c9dee
Florian Zouhar <florian.zouhar@igd.fraunhofer.de>
2019-08-20 12:54:35 +0200

    added test run

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from "./actions/store";
+
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  Map: () => ({}),
+}));
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const url = '/config';
+  const configPromise = fetch(url);
+  configPromise.then((res) => res.json())
+    .then(config => {
+    	ReactDOM.render(
+    		<Provider store={store}>
+            	<App config={config} />
+        	</Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
